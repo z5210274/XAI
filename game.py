5d840bd3383659e7aa11ca53ca9e3e7947d4e1f1
Chris Lee <z5210274@ad.unsw.edu.au>
2023-07-11 15:07:53 +1000

    CSV loading and hitbox implemented

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame, sys
 import math
-import json
+import os.path
+import csv
 
 from re import X
 from time import time
@@ -9,7 +10,7 @@ from projectile import *
  
 pygame.init()
 
-filename = 'data.json'
+filename = './data.csv'
 FPS = 240
 FramePerSec = pygame.time.Clock()
  
@@ -29,6 +30,21 @@ DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
 
+check_file = os.path.isfile(filename)
+print("data.csv exists: " + str(check_file))
+
+def write_csv(new_data):
+    field_names = ["Shooter_x_pos","Shooter_y_pos",
+                "Projectile_x_pos","Projectile_y_pos",
+                "Player_x_pos_current","Player_y_pos_current",
+                "Player_x_pos_initial","Player_y_pos_initial",
+                "Theta",
+                "Hit"]
+    dict = new_data
+    with open(filename, 'a') as file:
+        dict_object = csv.DictWriter(file, fieldnames=field_names, lineterminator = '\n') 
+        dict_object.writerow(dict)
+
 #################################################################################################
 #################################################################################################
 #################################################################################################
@@ -44,7 +60,20 @@ class Enemy(pygame.sprite.Sprite):
         self.y = self.rect.y
  
     def update(self):
-        self.image = pygame.transform.rotate(self.image, 180)
+        pressed_keys = pygame.key.get_pressed()
+
+        if self.rect.left > 0:
+              if pressed_keys[K_a]:
+                  self.rect.move_ip(-2, 0)
+        if self.rect.right < SCREEN_WIDTH:        
+              if pressed_keys[K_d]:
+                  self.rect.move_ip(2, 0)
+        if self.rect.top > 0:
+              if pressed_keys[K_w]:
+                  self.rect.move_ip(0, -2)
+        if self.rect.bottom < SCREEN_HEIGHT:        
+              if pressed_keys[K_s]:
+                  self.rect.move_ip(0, 2)
  
     def draw(self, surface):
         surface.blit(self.image, self.rect) 
@@ -98,6 +127,8 @@ class Projectile(pygame.sprite.Sprite):
         self.y_vel = math.sin(self.theta * (2*math.pi/360)) * 7
         self.x = self.initial_x
         self.y = self.initial_y
+        self.miss_x = -1
+        self.miss_y = -1
         self.game_area = game_area
 
         self.image = pygame.transform.scale(pygame.image.load("Bullet.png"), (20,20))
@@ -107,14 +138,50 @@ class Projectile(pygame.sprite.Sprite):
         self.rect.centerx = x
         self.rect.centery = y
 
-    def update(self):
+    def update(self, rect):
         self.x += self.x_vel
         self.y += self.y_vel
 
         self.rect.centerx = int(self.x)
         self.rect.centery = int(self.y)
 
+        if (self.theta + 90 > 360):
+            self.theta = self.theta - 360
+
+        if (rect.collidelistall([self.rect])):
+
+            data = {"Shooter_x_pos": self.initial_x, 
+                    "Shooter_y_pos": self.initial_y,
+                    "Projectile_x_pos": self.rect.centerx,
+                    "Projectile_y_pos": self.rect.centery, 
+                    "Player_x_pos_current": rect.centerx,
+                    "Player_y_pos_current": rect.centery,
+                    "Player_x_pos_initial": self.player_x,
+                    "Player_y_pos_initial": self.player_y,
+                    "Theta": round(self.theta + 90, 2),
+                    "Hit": 1}
+
+            write_csv(data)
+            self.kill()
+
+        if (self.rect.top > rect.bottom and self.miss_x == -1 and self.miss_y == -1):
+            self.miss_x = self.rect.centerx
+            self.miss_y = self.rect.centery
+
         if not self.game_area.contains(self.rect):
+
+            data = {"Shooter_x_pos": self.initial_x, 
+                    "Shooter_y_pos": self.initial_y,
+                    "Projectile_x_pos": self.miss_x,
+                    "Projectile_y_pos": self.miss_y, 
+                    "Player_x_pos_current": rect.centerx,
+                    "Player_y_pos_current": rect.centery,
+                    "Player_x_pos_initial": self.player_x,
+                    "Player_y_pos_initial": self.player_y,
+                    "Theta": round(self.theta + 90, 2),
+                    "Hit": 0}
+
+            write_csv(data)
             self.kill()
 
     def draw(self, surface):
@@ -131,6 +198,8 @@ projectile_group = pygame.sprite.Group()
 while True:     
     line = [(E1.rect.centerx, E1.rect.centery),(P1.rect.centerx, P1.rect.centery)]
     theta = getAngle(line[1], line[0])
+    if (theta > 360):
+        theta = theta - 360
     for event in pygame.event.get():              
         if event.type == QUIT:
             pygame.quit()
@@ -139,8 +208,8 @@ while True:
             if event.key == pygame.K_SPACE:
                 bullet = Projectile(E1.rect.centerx, E1.rect.centery, P1.rect.centerx, P1.rect.centery, theta)
                 projectile_group.add(bullet)
-                print(bullet.initial_x, bullet.initial_y, bullet.player_x, bullet.player_y, theta - 90)
-                print(bullet.x_vel, bullet.y_vel)
+                #print(bullet.initial_x, bullet.initial_y, bullet.player_x, bullet.player_y, theta - 90)
+                #print(bullet.x_vel, bullet.y_vel)
             if event.key == pygame.K_r: # Clear projectile cache
                 projectile_group.empty()
             if event.key == pygame.K_p:
@@ -154,7 +223,7 @@ while True:
     
     P1.update()
     #E1.update()
-    projectile_group.update()
+    projectile_group.update(P1.rect)
          
     pygame.display.update()
     FramePerSec.tick(FPS)
