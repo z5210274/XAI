9f02d4b62db20af1088d52ab050099b4b2135086
Chris Lee <z5210274@ad.unsw.edu.au>
2024-02-24 16:58:58 +1100

    Reco updated pipeling

--- reco.py
+++ reco.py
@@ -1,5 +1,11 @@
 # Apply my TCAV coefficients for a decomposition starter
-# Then continue with learning and convergence to find best reward decomposition
-# The coefficients will align to which Q-value performs best with the decomposition formula, then that will explain in human terms
-# E.g: If coefficients enemy initial pos is most significant for shoot action Q-Value, then we can explain the enemy has reached ideal position
-# Elsewise, if it's most significant for player_initial pos and final_pos for shot, then movement was a big factor for the decision
+# Then continue with learning and convergence to find best reward decomposition function
+# 1. Figure out a reward function that decomposes the rewards into sub rewards and classes them
+# 1a. Using HRA create n reward functions (n = variable of TCAV)
+#     which will hopefully represent each coefficent then train each one to sum of Hybrid Q-Value
+#     then the weights of each agent will depict their importance in the hybrid Q-Value
+#     The Hybrid Q-Value function will minimize loss of all n functions
+# 1b. Each n function will represent a specific reward type by omitting/emphasising a specific environment variable
+# Reward Decomposition explanation works by comparing the tradeoffs between sub rewards
+# 2. Compare the subrewards between each Action at a state to explain the tradeoff
+# 3. See if the tradeoffs between subrewards align with the TCAV coefficients to see which category of decision making
